e3e2d473a6565e230cb0299527c71b8a53bb051d
Bhaswanth <bhaswanth22@gmail.com>
2025-10-18 19:19:54 +0530

    Modified book form to apply tailwind

--- modules/PanelBooks/views/bookform.php
+++ modules/PanelBooks/views/bookform.php
@@ -1,29 +1,28 @@
-    <form action="<?= $formUrl ?>" id="article-form" method="POST" enctype="multipart/form-data">
+    <form action="<?= $formUrl ?>" id="article-form" class="panel-form" method="POST" enctype="multipart/form-data">
         <?= csrf_field() ?>
         <?php $template->includes("includes/messages", null, "Auth") ?>
-        <div class="row">
-            <div class="col-md-8">
-                <div class="mb-3">
+        <div class="grid grid-cols-12 gap-2">
+            <div class="md:col-span-8">
+                <div class="mb-3 border-b border-solid border-dark-400 py-2">
                     <input type="text" class="article-title" name="title" id="title" value="<?= $title ?? old('title') ?>" placeholder="Title" />
                 </div>
-                <hr />
                 <div class="mb-3">
                     <div id="editor-container" class="editor-container"></div>
                     <input type="hidden" name="description" id="editor-content" value='<?= $description ?? old('description') ?>' />
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card accordion">
-                    <div class="card-header accordion-header cursor-pointer">
+            <div class="md:col-span-4">
+                <div class="card">
+                    <div class="card-header">
                         <h6 class="card-title">Article Settings</h6>
                     </div>
-                    <div class="card-body accordion-body">
-                        <div class="mb-3">
+                    <div class="card-body">
+                        <div class="form-group">
                             <label class="form-label" for="url">Slug</label>
                             <input type="text" class="form-control" name="slug" id="slug" value="<?= $slug ?? old('slug') ?>" />
                         </div>
 
-                        <div class="mb-3">
+                        <div class="form-group">
                             <label class="form-label" for="tags">Tags</label>
                             <input type="text" class="form-control no-resize" rows="2" name="tags" id="tags" value="<?= $tags ?? old('tags') ?>" />
                             <span class="label-xs">Seperate tags with comma(,)</span>
@@ -31,13 +30,15 @@
                     </div>
                 </div>
                 <br />
-                <div class="card accordion">
-                    <div class="card-header accordion-header cursor-pointer">
+                <div class="card">
+                    <div class="card-header">
                         <h6 class="card-title">Media</h6>
                     </div>
-                    <div class="card-body <?= !isset($banner_url) ? 'collapse' : '' ?> accordion-body">
-                        <label class="form-label" for="banner-image">Banner Image</label>
-                        <input type="file" class="form-control" accept="image/*" name="banner" id="banner-image" />
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label class="form-label" for="banner-image">Banner Image</label>
+                            <input type="file" class="form-control" accept="image/*" name="banner" id="banner-image" />
+                        </div>
 
                         <div class="" id="banner-preview" <?= isset($banner_url) ? 'style="display: block;"' : '' ?>>
                             <img src="<?= $banner_url ?>" id="preview" alt="Banner Preview" />
@@ -45,22 +46,22 @@
                     </div>
                 </div>
                 <br/>
-                <div class="card accordion">
-                    <div class="card-header accordion-header cursor-pointer">
+                <div class="card">
+                    <div class="card-header">
                         <h6 class="card-title">Meta Data (Optional)</h6>
                     </div>
-                    <div class="card-body collapse accordion-body">
-                        <div class="mb-3">
+                    <div class="card-body">
+                        <div class="form-group">
                             <label class="form-label" for="meta-title">Meta Title</label>
                             <input type="text" class="form-control" name="meta_title" id="meta-title" value="<?= $meta_title ?? old('meta_title') ?>" />
                         </div>
 
-                        <div class="mb-3">
+                        <div class="form-group">
                             <label class="form-label" for="meta-description">Meta Description</label>
                             <textarea class="form-control no-resize" rows="3" name="meta_description" id="meta-description"><?= $meta_description ?? old('meta_description') ?></textarea>
                         </div>
 
-                        <div class="md-3">
+                        <div class="form-group">
                             <label class="form-label" for="meta-tags">Meta Tags</label>
                             <input type="text" class="form-control no-resize" rows="2" name="meta_tags" id="meta-tags" value="<?= $meta_tags ?? old('meta_tags') ?>" />
                         </div>
